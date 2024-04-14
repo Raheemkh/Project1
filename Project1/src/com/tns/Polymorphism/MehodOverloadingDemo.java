c7a281a94cd87b7a608f9c6752c482f3deb7f255
DELL <DELL@DESKTOP-CO0D26T>
2024-04-14 09:53:24 -0500

    First commit

---
+++ Project1/src/com/tns/Polymorphism/MehodOverloadingDemo.java
@@ -0,0 +1,12 @@
+package com.tns.Polymorphism;
+
+public class MehodOverloadingDemo {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	Operation obj=new Operation();
+	obj.addition();
+	obj.addition(2, 4);
+		}
+
+	}
